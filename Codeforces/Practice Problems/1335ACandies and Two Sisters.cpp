6fc359f601669c182a540f4caba7da5c5acbfddb
Isam David <isamdavid17@gmail.com>
2020-09-29 20:05:30 -0500

    Add solution

---
+++ Codeforces/Practice Problems/1335ACandies and Two Sisters.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t,n,z; cin>>t;
+  while(t--){
+    cin>>n;
+      z=(n-1)/2;
+      if(z>0) cout<<z<<endl;
+      else cout<<0<<endl;
+    }
+  return 0;
+}
